8b35121d0429fa758e296b90e5a5afea1b6c8d1a
EKinker <136768892+EKinker@users.noreply.github.com>
2023-07-09 15:40:30 -0400

    Firsttry (#1)
    * Laid out structure
    
    * partial methods completed
    
    * confused
    
    * Something is working as intended
    
    * I'm stuck but here's a push
    
    * works until the play again loop

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,5 +1,8 @@
 package com.github.zipcodewilmington;
 
+import java.util.Random;
+import java.util.Scanner;
+
 
 /**
  * @author xt0fer
@@ -7,4 +10,139 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+
+    public static void main(String[] args) {
+        Hangman game = new Hangman();
+        game.runGame();
+    }
+
+    //instance variables    //use to sync all the game state
+    public static boolean playGame = true;
+
+    //array of strings of words player could guess
+    String[] wordList = {"alphabet", "breakfast", "cockatiel", "dragonfly", "enormous", "figurehead", "gathering"};
+    static int guessesLeft;
+    String wordToGuess = wordList[new Random().nextInt(wordList.length)]; //game picks a random word //Use Random class
+    char[] lettersInWord = new char[wordToGuess.length()];
+    public void runGame() {   //all output and input should be in specialized methods, not in runGame()
+        //int randomInd = new Random().nextInt(wordList.length);  //possibly remove
+        announceGame();
+
+        //while keep playing?  (outer loop)
+        while (playGame) {
+            System.out.println(wordToGuess);  //test.  Remove later
+
+            initializeGameState();
+            //start game
+            //set word guessed to false
+            boolean isGuessed = false;
+            // while word isn't guessed (inner loop)
+            while (!isGuessed && guessesLeft > 0) {
+                printCurrentState();  //print current game state
+                System.out.print("Guess a letter: ");
+                process(getPlayerInput()); //ask for a guess (1 letter) and check for letter in word
+                isGuessed = isWordGuessed();
+
+            }
+                if (isGuessed){
+                    playerWon();
+                }
+                if (guessesLeft == 0){
+                    playerLost();
+                }
+
+            // play again?
+            askToPlayAgain();
+        }
+        gameOver();//game over
+    }
+
+    public static String printArray(char[] array) { // print out characters from the array with a space between
+        String spacedWord = "";
+        //for loop
+        for (int i = 0; i < array.length; i++) {
+            spacedWord += (array[i]) + " ";
+        }
+        return spacedWord;
+    }
+
+    public void announceGame() { //print welcome
+        System.out.println("Welcome to Hangman!  Or \"Word Guess\" or whatever.  Let's play!");
+    }
+
+    public void gameOver() { //if no longer playing
+        System.out.println("Game Over. Thank you for playing!");
+        playGame =false;
+    }
+
+    public void initializeGameState() { //sets up char[] for secret word and guesses
+        for (int i = 0; i < lettersInWord.length; i++){
+            lettersInWord[i] = '_';
+        }
+        guessesLeft = wordToGuess.length();
+
+    }
+
+    public static char getPlayerInput() { //returns a char from player input   //use Scanner class
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        char letter = input.toLowerCase().charAt(0);
+
+        return letter;
+    }
+
+
+
+    public boolean isWordGuessed() {  //returns boolean
+        boolean game = true;
+        for (int i = 0; i < lettersInWord.length; i ++) {
+            if (lettersInWord[i] == '_') {
+                game = false;
+            }
+        }
+        return game;
+    }
+
+    public void askToPlayAgain() {  //returns boolean
+        System.out.print("Would you like to play again?");
+        char response = getPlayerInput();
+        if (response == 'n') {
+            playGame = false;
+        }
+
+
+    }
+
+    public void printCurrentState() {  //uses printArray to show player where they are at
+        System.out.println(printArray(lettersInWord));
+        System.out.println(remainingGuesses());
+    }
+
+    private static String remainingGuesses() {
+        String playsLeft = "Guesses Remaining: ";
+        for (int i = 0; i< guessesLeft; i++ ) {
+            playsLeft+="X";
+        }
+        return playsLeft;
+    }
+
+    public void process(char input) {  //loops through the word array, looking for the input guess, replaces the "_" with the guessed char if found
+        boolean correctGuess = false;
+        for (int i = 0; i < wordToGuess.length(); i ++){
+            char ch = wordToGuess.charAt(i);
+            if (ch == input) {
+                lettersInWord[i]= ch;
+                correctGuess = true;
+            }
+        }
+        if (!correctGuess) { guessesLeft--;}
+    }
+
+    public void playerWon() { //    :)
+        System.out.println("You guessed the word!");
+    }
+
+    public void playerLost() { //   :(
+        System.out.println("Oh no! You didn't guess the word in time. The word was \'" +wordToGuess + "\'. Womp Womp :(");
+    }
 }
--- src/test/java/com/github/zipcodewilmington/HangmanTest.java
+++ src/test/java/com/github/zipcodewilmington/HangmanTest.java
@@ -1,9 +1,54 @@
 package com.github.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class HangmanTest {
+
+    @Test
+    public void testPrintArray() {
+        char[] array = {'h','a','n','g','m','a','n'};
+        String actual = Hangman.printArray(array);
+        String expected = "h a n g m a n ";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    public void testAnnounceGame() {
+    }
+
+    public void testGameOver() {
+    }
+
+    public void testInitializeGameState() {
+    }
+
+    //@Test
+    public void testGetNextGuess() {
+       // String guess = Hangman.getNextGuess("guess");
+
+    }
+
+    public void testIsWordGuessed() {
+    }
+
+    public void testAskToPlayAgain() {
+    }
+
+    public void testPrintCurrentState() {
+    }
+
+    public void testProcess() {
+    }
+
+    public void testPlayerWon() {
+    }
+
+    public void testPlayerLost() {
+    }
 }
