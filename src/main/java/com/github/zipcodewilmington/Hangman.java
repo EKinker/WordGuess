217b81159bfaf3fd88e1fd35a63b578e0a305dc9
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-09 12:48:35 -0400

    starting over

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -7,4 +7,25 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+
+    public static void main(String[] args) {
+        Hangman hangman = new Hangman();
+        hangman.runGame();
+    }
+
+    //instance variables
+    String[] wordList = {"arrogant", "beautiful", "claustrophobic", "dragonfly", "exorcist"};
+    int guessesRemaining= 0;
+
+    public void runGame(){
+        welcome();
+
+
+    }
+
+
+    private static void welcome() {
+        System.out.println("Welcome to Word Guess.  Let's play!");
+        System.out.println("Try to guess my word:");
+    }
 }
